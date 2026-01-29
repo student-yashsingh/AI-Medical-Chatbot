5f450f0ac689a1e1c59ee20ba23aeadfff50348c
student-yashsingh <yash1243singh@gmail.com>
2026-01-29 19:36:26 +0530

    setting the requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_pckages,setup
+
+setup(
+    name="AI_Medical_Chatbot",
+    version="0.1.0", 
+    author="Yash Singh",
+    author_email="yash1243singh@gmail.com",
+    packages=find_pckages(),
+    install_requires=[]
+)
